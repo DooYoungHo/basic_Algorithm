a1c79dd4b6781f00165baff472ce499c1238d1c5
DooYoungHo <129814321+DooYoungHo@users.noreply.github.com>
2023-04-15 00:32:52 +0900

    add : basic_py

---
+++ basic_algorithm.py
@@ -0,0 +1,15 @@
+# add
+a, b = map(int, input().split())
+print(a + b)
+
+# minus
+a, b = map(int, input().split())
+print(a - b)
+
+# mul
+a, b = map(int, input().split())
+print(a * b)
+
+# div
+a,b = map(int, input().split())
+print(a / b)
